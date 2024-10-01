1c2e59412ee2441925b5a50ef49dd0c4d4186b3b
chienpromax <xuanchient033@gmail.com>
2024-10-01 21:21:49 +0700

    first commit

---
+++ src/test/java/yourstyle/com/shope/ShopeApplicationTests.java
@@ -0,0 +1,13 @@
+package yourstyle.com.shope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

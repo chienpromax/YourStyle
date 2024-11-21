c3e91325c33f4d1038e0fc5725b5d4bc32f268fb
chienpromax <xuanchient033@gmail.com>
2024-11-21 13:51:40 +0700

    Thêm file AddressDto.java và VoucherDTO.java

---
+++ src/main/java/yourstyle/com/shope/validation/admin/AddressDto.java
@@ -0,0 +1,62 @@
+package yourstyle.com.shope.validation.admin;
+
+import java.io.Serializable;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import yourstyle.com.shope.model.Customer;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressDto implements Serializable {
+    private Integer addressId;
+    @NotBlank(message = "Vui lòng nhập địa chỉ cụ thể!")
+    private String street;
+    private String ward;
+    private String district;
+    private String city;
+    private Boolean isDefault = false;
+    private Integer customerId;
+    private Customer customer;
+    // thêm khách hàng sell
+    private String fullname;
+    private String phoneNumber;
+    private boolean gender;
+    private String email;
+
+    public AddressDto(Integer addressId, String street, String ward, String district, String city, Customer customer,
+    Boolean isDefault) {
+        this.addressId = addressId;
+        this.street = street;
+        this.ward = ward;
+        this.district = district;
+        this.city = city;
+        this.customer = customer;
+        this.isDefault = isDefault;
+    }
+
+    public AddressDto(String fullname, String phoneNumber, boolean gender, String street, String ward, String district,
+            String city) {
+        this.fullname = fullname;
+        this.phoneNumber = phoneNumber;
+        this.gender = gender;
+        this.street = street;
+        this.ward = ward;
+        this.district = district;
+        this.city = city;
+    }
+
+    // chọn khách hàng
+    public AddressDto(String fullname, String phoneNumber, String street, String ward, String district,
+            String city) {
+        this.fullname = fullname;
+        this.phoneNumber = phoneNumber;
+        this.street = street;
+        this.ward = ward;
+        this.district = district;
+        this.city = city;
+    }
+}
---
+++ src/main/java/yourstyle/com/shope/validation/admin/VoucherDTO.java
@@ -0,0 +1,80 @@
+package yourstyle.com.shope.validation.admin;
+
+import jakarta.validation.constraints.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import java.util.List;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.sql.Timestamp;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import yourstyle.com.shope.model.Account;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VoucherDTO {
+
+    private Integer voucherId;
+
+    @NotBlank(message = "Mã giảm giá không được để trống")
+    @Size(max = 255, message = "Voucher code không vượt quá 255 ký tự")
+    private String voucherCode;
+
+    @NotBlank(message = "Tên giảm giá không được để trống")
+    @Size(max = 255, message = "Tên giảm giá không vượt quá 255 ký tự")
+    private String voucherName;
+
+    @Size(max = 1000, message = "Mô tả không được dài hơn 1000 ký tự")
+    private String description;
+
+    @NotNull(message = "Giảm giá không được để trống")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Giảm giá phải lớn hơn 0")
+    private BigDecimal discountAmount;
+
+    @NotNull(message = "Giá trị thấp nhất không được để trống")
+    @DecimalMin(value = "0.0", inclusive = false, message = "Giá trị thấp nhất phải lớn hơn 0")
+    private BigDecimal minTotalAmount;
+
+    @DecimalMin(value = "0.0", message = "Giá trị lớn nhất phải lớn hơn hoặc bằng 0")
+    private BigDecimal maxTotalAmount;
+
+    @NotNull(message = "Số lượng không được để trống")
+    @Min(value = 1, message = "Số lượng phải lớn hơn hoặc bằng 1")
+    private Integer maxUses;
+
+    @NotNull(message = "Số lần sử dụng không được để trống")
+    @Min(value = 1, message = "Số lần sử dụng phải lớn hơn hoặc bằng 1")
+    private Integer maxUsesUser;
+
+    @NotNull(message = "Loại giảm giá không được để trống")
+    private Byte type;
+
+    @NotNull(message = "Ngày bắt đầu không được để trống")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime startDate;
+
+    @NotNull(message = "Ngày kết thúc không được để trống")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm")
+    private LocalDateTime endDate;
+
+    private Timestamp createAt = new Timestamp(System.currentTimeMillis());
+
+    private Boolean isPublic;
+
+    private List<Integer> customerIds;
+
+    private Account account;
+
+    private String formattedDiscount;
+
+    public VoucherDTO(String voucherCode, Byte type, String formattedDiscount) {
+        this.voucherCode = voucherCode;
+        this.type = type;
+        this.formattedDiscount = formattedDiscount;
+    }
+
+}

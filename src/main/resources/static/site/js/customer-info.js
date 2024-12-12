faff37ff073ca0f1a37864cf988f0fd19da2ecfa
chienpromax <xuanchient033@gmail.com>
2024-12-12 19:45:39 +0700

    update controll video

--- src/main/resources/static/site/js/customer-info.js
+++ src/main/resources/static/site/js/customer-info.js
@@ -28,33 +28,31 @@ document.addEventListener("click", function (event) {
   }
 });
 
-document.querySelectorAll('.video-controls').forEach(control => {
-  const videoId = control.getAttribute('data-video');
-  const video = document.getElementById(videoId);
+document.addEventListener("DOMContentLoaded", function () {
+  const video = document.getElementById("video3");
+  const playPauseIcon = document.getElementById("playPauseIcon1");
+  const muteUnmuteIcon = document.getElementById("muteUnmuteIcon1");
 
-  const playPauseIcon = control.querySelector('.fas.fa-pause, .fas.fa-play');
-  const muteUnmuteIcon = control.querySelector('.fas.fa-volume-mute, .fas.fa-volume-up');
-
-  playPauseIcon.addEventListener('click', () => {
+  playPauseIcon.addEventListener("click", function () {
       if (video.paused) {
           video.play();
-          playPauseIcon.classList.remove('fa-play');
-          playPauseIcon.classList.add('fa-pause');
+          playPauseIcon.classList.remove("fa-play");
+          playPauseIcon.classList.add("fa-pause");
       } else {
           video.pause();
-          playPauseIcon.classList.remove('fa-pause');
-          playPauseIcon.classList.add('fa-play');
+          playPauseIcon.classList.remove("fa-pause");
+          playPauseIcon.classList.add("fa-play");
       }
   });
 
-  muteUnmuteIcon.addEventListener('click', () => {
+  muteUnmuteIcon.addEventListener("click", function () {
       video.muted = !video.muted;
       if (video.muted) {
-          muteUnmuteIcon.classList.remove('fa-volume-up');
-          muteUnmuteIcon.classList.add('fa-volume-mute');
+          muteUnmuteIcon.classList.remove("fa-volume-up");
+          muteUnmuteIcon.classList.add("fa-volume-mute");
       } else {
-          muteUnmuteIcon.classList.remove('fa-volume-mute');
-          muteUnmuteIcon.classList.add('fa-volume-up');
+          muteUnmuteIcon.classList.remove("fa-volume-mute");
+          muteUnmuteIcon.classList.add("fa-volume-up");
       }
   });
 });
